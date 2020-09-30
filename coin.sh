261b06c99122ad92031545251abdfde1e5cffc7f
priyankarai11 <priyankakumbra@gmail.com>
2020-09-30 12:19:36 -0700

    Added:Counting head and tail based on flip

--- coin.sh
+++ coin.sh
@@ -7,3 +7,16 @@ then
 else
 	echo "Tails"
 fi
+
+for i in $(seq 10)
+do
+arr=$((RANDOM%2))
+if [ $arr -eq 0 ]
+then
+	head=$(($head+1))
+else
+	tail=$(($tail+1))
+fi
+done
+echo "Head won $head times"
+echo "Tails won $tail times"
